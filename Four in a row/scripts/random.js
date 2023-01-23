f1621e15e5ee7a2894ca568b4a37f2eab4b3868c
George Zeytunyan <47988040+zeytunyan@users.noreply.github.com>
2023-01-23 04:02:39 +0300

    Delete Four in a row directory

--- Four in a row/scripts/random.js
+++
@@ -1,9 +0,0 @@
-function getRandomColors() {
-    return getRand(0, 1) ? ["gold", "red"] : ["red", "gold"];
-}
-
-function getRand(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
